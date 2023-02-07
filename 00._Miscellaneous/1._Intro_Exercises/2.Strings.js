ca6f1dd5adc3d4806ac7dd7a08d09ade94430ebd
xenius <xeni0103@stud.kea.dk>
2023-02-07 19:22:07 +0100

    Commit for assagniment week 1

---
+++ 00._Miscellaneous/1._Intro_Exercises/2.Strings.js
@@ -0,0 +1,61 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+const resultOne = Number(numberOne) + Number(numberTwo);
+console.log(resultOne);
+
+// --------------------------------------
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+const resultTwo = Number(anotherNumberOne) + Number(anotherNumberTwo);
+console.log(resultTwo.toFixed(2));
+
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+const resultThree = (one + two + three) / 3;
+console.log(resultThree.toFixed(5));
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+
+const resultFour = letters.charAt(2);
+console.log(resultFour);
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+
+const resultFive = fact.replace("j", "J");
+console.log(resultFive);
+
+
+// --------------------------------------
+
+
+
