437ffb835ef778ae01fdc3fbe15137244606ccfd
xenius <xeni0103@stud.kea.dk>
2023-02-08 09:29:02 +0100

    Finished all the exercises for week 1

--- 00._Miscellaneous/1._Intro_Exercises/3.Objects.js
+++ 00._Miscellaneous/1._Intro_Exercises/3.Objects.js
@@ -7,11 +7,15 @@ const myObj = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
+console.log(myObj.message);
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
 
+const personObj = { name: "Xenius", age: 26 };
+console.log(personObj);
 
 // --------------------------------------
 // Exercise 3 - Add a property 
@@ -20,6 +24,9 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+console.log(stackOverflow);
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
@@ -27,6 +34,9 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = "Just a tribute";
+console.log(thisSong);
 
 // --------------------------------------
 
--- 00._Miscellaneous/1._Intro_Exercises/4.Arrays.js
+++ 00._Miscellaneous/1._Intro_Exercises/4.Arrays.js
@@ -6,6 +6,7 @@
 const letters = ["a","b","c"];
 // show b in the console 
 
+console.log(letters[1]);
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
@@ -14,6 +15,8 @@ const friends = [];
 
 // What a lonely array. Add at least 3 friend objects to it.  
 
+friends.push("Egon", "Benny", "Kjeld");
+console.log(friends);
 
 // --------------------------------------
 // Exercise 3 - Get the index of first occurance of that value. 
@@ -23,6 +26,8 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
 
+const theIndex = significantMathNumbers.indexOf(1729);
+console.log(theIndex);
 
 // --------------------------------------
 // Exercise 4 - Inserting elements
@@ -31,8 +36,8 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
 
-
-
+diet.splice(2, 0, "hamburger", "soda", "pizza");
+console.log(diet);
 
 // --------------------------------------
 // Exercise 5 - Remove element
@@ -40,16 +45,16 @@ const diet = ["tomato", "cucumber", "rocket"];
 // Remove the LAST element of the array.
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
 
-
-
-
+diet.pop();
+console.log(diet);
 
 // --------------------------------------
 // Exercise 6 - Copy array
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
-
+const dinnerTray = Array.from(diet);
+console.log("Dinner Tray: " + dinnerTray);
 
 // --------------------------------------
 // Exercise 7 - For loop
@@ -57,8 +62,11 @@ const diet = ["tomato", "cucumber", "rocket"];
 const lettersExpanded = ["a","b","c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
-
-
+let counter = 0;
+lettersExpanded.forEach( (element) => { 
+    if(counter % 2 === 1) console.log(element);
+    counter++;
+});
 
 // --------------------------------------
 // Exercise 8 - For loop and if statement
@@ -69,6 +77,15 @@ const discardedNumbers = [];
 
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
+
+numbers.forEach( (element) => {
+    if(element < 0 || element > 6){
+        console.log(element);
+    } else {
+        discardedNumbers.push(element);
+    };
+});
+console.log(discardedNumbers);
 
 // --------------------------------------
 
